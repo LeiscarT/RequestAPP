23ba04e32e4e2b6494341295780a34c5466c367d
LeiscarT <leiscartp29@gmail.com>
2021-07-29 16:52:25 -0400

    Add SolicitudController

---
+++ RequestAPP/RequestAPP/Controllers/SolicitudController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RequestAPP.Models;
+
+namespace RequestAPP
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolicitudController : ControllerBase
+    {
+        private readonly RequestContext _context;
+
+        public SolicitudController(RequestContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Solicitud
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Solicitud>>> GetSolicitud()
+        {
+            return await _context.Solicitud.ToListAsync();
+        }
+
+        // GET: api/Solicitud/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Solicitud>> GetSolicitud(int id)
+        {
+            var solicitud = await _context.Solicitud.FindAsync(id);
+
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            return solicitud;
+        }
+
+        // PUT: api/Solicitud/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSolicitud(int id, Solicitud solicitud)
+        {
+            if (id != solicitud.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(solicitud).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SolicitudExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Solicitud
+        [HttpPost]
+        public async Task<ActionResult<Solicitud>> PostSolicitud(Solicitud solicitud)
+        {
+            _context.Solicitud.Add(solicitud);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSolicitud", new { id = solicitud.Id }, solicitud);
+        }
+
+        // DELETE: api/Solicitud/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSolicitud(int id)
+        {
+            var solicitud = await _context.Solicitud.FindAsync(id);
+            if (solicitud == null)
+            {
+                return NotFound();
+            }
+
+            _context.Solicitud.Remove(solicitud);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SolicitudExists(int id)
+        {
+            return _context.Solicitud.Any(e => e.Id == id);
+        }
+    }
+}
