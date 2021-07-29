5c05094714e42431fb321464db66978e8dfce555
LeiscarT <leiscartp29@gmail.com>
2021-07-29 14:06:23 -0400

    Add model base (Persona, Solicitud, Modelo)

---
+++ RequestAPP/RequestAPP/Models/Estado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RequestAPP.Models
+{
+    public class Estado
+    {
+        public enum Estados
+        {
+            Abiertas,
+            Aprobadas,
+            Canceladas
+        }
+    }
+}
